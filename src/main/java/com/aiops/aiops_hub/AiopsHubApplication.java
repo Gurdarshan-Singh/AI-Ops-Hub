2d0f6148d2a1cdfe097843e0673c844ae29263e7
Gurdarshan Singh <gurdarshansingh@mac.home>
2026-05-31 18:05:09 -0400

    Initial Commit

---
+++ src/main/java/com/aiops/aiops_hub/AiopsHubApplication.java
@@ -0,0 +1,13 @@
+package com.aiops.aiops_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiopsHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiopsHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiops/aiops_hub/AiopsHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiops.aiops_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiopsHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
